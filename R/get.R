f240627cbb31ad92f38aa742993a3d51a79bea7f
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-10-11 05:11:11 +0200

    feat: get object

---
+++ R/get.R
@@ -0,0 +1,9 @@
+#' Get DataSHIELD Object
+#'
+#' @param object The object to be returned
+#' @return The input object, returned to the client side
+#' @description Returns the object to the client side for debugging purposes
+#' @export
+debugGetDS <- function(object) {
+  return(object)
+}
